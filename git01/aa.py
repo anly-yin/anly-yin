2580344fb0c85b20bd43386e4ec7f14bd40f97da
anly-yin <554764706@qq.com>
2022-02-10 19:43:45 +0800

    l

---
+++ git01/aa.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from time import sleep
+
+driver=webdriver.Chrome()
+driver.get("https://yxgl-ncdn1.ios.shenshouwl.com/yxgl/h5/index.html?p=gk&ya_game=1113&ya_sn=shenshou_h5&ya_sv=1.0&ya_ssv=V1_0")
